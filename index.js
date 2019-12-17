747b196c340da7cc28071304c2f3e91517ecde18
PianoFF <Fangfei1star@gmail.com>
2019-12-17 21:00:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+const mapToNegativize = function(ary){
+    let result = [ ];
+    ary.forEach(item => result.push(item * -1));
+    return result; 
+};
+
+const mapToDouble = function(ary){
+    let result = [ ];
+    ary.forEach(item => result.push(item * 2));
+    return result; 
+};
+
+const mapToNoChange = function(ary){
+    let result = [ ];
+    ary.forEach(item => result.push(item));
+    return result; 
+};
+
+const mapToSquare = function(ary){
+    let result = [ ];
+    ary.forEach(item => result.push(item ** 2));
+    return result; 
+};
+
+const reduceToTotal = function(ary, startingPoint = 0){
+    let result = parseInt(startingPoint);
+    ary.forEach(item => result +=item);
+    return result;
+};
+
+const reduceToAllTrue = function(ary){
+    let returnValue = true;  
+    ary.forEach(item => returnValue = (Boolean(item)) && returnValue);
+    return returnValue; 
+};
+
+const reduceToAnyTrue = function(ary){
+    let returnValue = false;
+    for (let i = 0; i < ary.length; i++){
+        if (Boolean(ary[i])){
+            returnValue = true;
+            break 
+        }
+    }
+    return returnValue;
+};
+
+/
